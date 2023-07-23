df210f631faa87fd669e6452a8f3479a65fa96fb
Ryan Khetlyr <ryan_khetlyr@waed.circ9.dcn>
2023-07-22 18:41:49 -0700

    ready to start part 2 - Normalization

--- src/main/app/application.cljs
+++
@@ -1,6 +0,0 @@
-(ns app.application
-  (:require
-    [com.fulcrologic.fulcro.application :as app]
-    [com.fulcrologic.fulcro.networking.http-remote :as http]))
-
-(defonce app (app/fulcro-app {:remotes {:remote (http/fulcro-http-remote {})}}))
--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -1,25 +1,24 @@
 (ns app.client
   (:require
-    [app.application :refer [app]]
-    [app.ui :as ui]
-    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.application :as app]
-    [com.fulcrologic.fulcro.data-fetch :as df]))
+    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
+    [com.fulcrologic.fulcro.dom :as dom]
+    [com.fulcrologic.fulcro.algorithms.merge :as merge]
+    [com.fulcrologic.fulcro.algorithms.data-targeting :as targeting]))
 
+(defsc Sample [this {:root/keys [people]}]
+       {}
+       (dom/div "TODO"))
 
-(defn ^:export init
-      "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
-      []
-      (app/mount! app ui/Root "app")
-      (df/load! app :friends ui/PersonList)
-      (df/load! app :enemies ui/PersonList)
-      (js/console.log "Loaded"))
+(defonce APP (app/fulcro-app))
 
-(defn ^:export refresh
-      "During development, shadow-cljs will call this on every hot reload of source. See shadow-cljs.edn"
-      []
-      ;; re-mounting will cause forced UI refresh, update internals, etc.
-      (app/mount! app ui/Root "app")
-      ;; As of Fulcro 3.3.0, this addition will help with stale queries when using dynamic routing:
-      (comp/refresh-dynamic-queries! app)
-      (js/console.log "Hot reload"))
+(defn ^:export init []
+      (app/mount! APP Sample "app"))
+
+(defn f [x]
+  (+ 1 2)
+  (* x x))
+
+(comment
+
+)
