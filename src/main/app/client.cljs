82f18e207988ddbb0fded895b4643cda6ebbfb20
Ryan Khetlyr <ryan_khetlyr@waed.circ9.dcn>
2023-08-06 13:16:11 -0700

    add detail table join to thing

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -6,8 +6,12 @@
     [com.fulcrologic.fulcro.algorithms.merge :as merge]
     [com.fulcrologic.fulcro.algorithms.data-targeting :as targeting]))
 
-(defsc Thing [this {:thing/keys [id name year] :as props}]
-  {:query [:thing/id :thing/name :thing/year]
+(defsc Detail [this {:detail/keys [id type year manufacturer breed] :as props}]
+  {:query [:detail/id :detail/type :detail/year :detail/manufacturer :detail/breed]
+   :ident :detail/id})
+
+(defsc Thing [this {:thing/keys [id name year detail] :as props}]
+  {:query [:thing/id :thing/name :thing/year {:thing/detail (comp/get-query Detail)}]
    :ident :thing/id})
 
 (defsc ThingCategory [this {:thing-category/keys [id name things] :as props}]
@@ -26,6 +30,41 @@
 
 ; go from the bottom up into the repl
 (comment
+  (app/current-state APP)
+  (swap! (::app/state-atom APP) assoc-in [:detail/id 1 :detail/manufacturer] "Honda")
+  (app/current-state APP)
+  (swap! (::app/state-atom APP) assoc-in [:detail/id 1 :detail/year] 2014)
+  (app/current-state APP)
+  (swap! (::app/state-atom APP) assoc-in [:thing/id 5 :thing/name] "Civic")
+  (app/current-state APP)
+  (swap! (::app/state-atom APP) assoc-in [:thing/id 2 :thing/name] "Switch")
+
+  (app/current-state APP)
+  ; detail: id type year manufacturer breed
+  (merge/merge-component! APP Detail {:detail/id 4
+                                      :detail/type "Video Game System"
+                                      :detail/manufacturer "Nintendo"
+                                      :detail/year 2023}
+                          :append [ :thing/id 2 :thing/detail])
+  (app/current-state APP)
+  ; detail: id type year manufacturer breed
+  (merge/merge-component! APP Detail {:detail/id 3
+                                      :detail/type "Motorcycle"
+                                      :detail/manufacturer "Harley-Davidson"
+                                      :detail/year 1990}
+                          :append [ :thing/id 3 :thing/detail])
+  (app/current-state APP)
+  ; detail: id type year manufacturer breed
+  (merge/merge-component! APP Detail {:detail/id 2
+                                      :detail/type "Cow"
+                                      :detail/breed "Guernsey"}
+                          :append [ :thing/id 4 :thing/detail])
+  (app/current-state APP)
+  ; detail: id type year manufacturer breed
+  (merge/merge-component! APP Detail {:detail/id 1
+                                     :detail/type "Car"}
+                          :append [ :thing/id 5 :thing/detail])
+
   (app/current-state APP)
   (swap! (::app/state-atom APP) assoc-in [:thing/id 3 :thing/year] 1990)
   (swap! (::app/state-atom APP) assoc-in [:thing/id 1 :thing/year] 2023)
