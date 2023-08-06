a4a7adf76fb3a9eeae15b27e66fca61748c7890d
Ryan Khetlyr <ryan_khetlyr@waed.circ9.dcn>
2023-08-06 10:21:17 -0700

    thing-categories have things

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -6,13 +6,13 @@
     [com.fulcrologic.fulcro.algorithms.merge :as merge]
     [com.fulcrologic.fulcro.algorithms.data-targeting :as targeting]))
 
-(defsc ThingCategory [this {:thing-category/keys [id name] :as props}]
-  {:query [:thing-category/id :thing-category/name]
-   :ident :thing-category/id})
-
 (defsc Thing [this {:thing/keys [id name] :as props}]
   {:query [:thing/id :thing/name]
    :ident :thing/id})
+
+(defsc ThingCategory [this {:thing-category/keys [id name things] :as props}]
+  {:query [:thing-category/id :thing-category/name {:thing-category/things (comp/get-query Thing)}]
+   :ident :thing-category/id})
 
 
 (defsc MyThings [this {:root/keys [thing-category]}]
@@ -26,7 +26,24 @@
 
 ; go from the bottom up into the repl
 (comment
-  #_(merge/merge-component! APP Thing {:thing/id 1
+  (app/current-state APP)
+  (merge/merge-component! APP Thing {:thing/id 5
+                                     :thing/name "Honda Civic"}
+                          :append [ :thing-category/id 1 :thing-category/things])
+  (app/current-state APP)
+  (merge/merge-component! APP Thing {:thing/id 4
+                                     :thing/name "Morna"}
+                          :append [ :thing-category/id 2 :thing-category/things])
+  (app/current-state APP)
+  (merge/merge-component! APP Thing {:thing/id 3
+                                     :thing/name "Motorcycle"}
+                          :append [ :thing-category/id 1 :thing-category/things])
+  (app/current-state APP)
+  (merge/merge-component! APP Thing {:thing/id 2
+                                     :thing/name "Nintendo Switch"}
+                          :append [ :thing-category/id 3 :thing-category/things])
+  (app/current-state APP)
+  (merge/merge-component! APP Thing {:thing/id 1
                                      :thing/name "Trumpet"}
                           :append [ :thing-category/id 3 :thing-category/things])
   (app/current-state APP)
