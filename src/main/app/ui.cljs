200eca52971034a87723a1f89b422cbcdd06fd9e
Ryan Khetlyr <ryan_khetlyr@waed.circ9.dcn>
2023-09-28 08:33:16 -0700

    added :rebels to initial state

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -20,11 +20,15 @@
    (fn [{:keys [id label]}]
      {:list/id     id
       :list/label  label
-      :list/people (if (= id :friends)
+      :list/people (cond (= id :friends)
                      [(comp/get-initial-state Person {:id 1 :name "Sally" :age 32})
                       (comp/get-initial-state Person {:id 2 :name "Joe" :age 22})]
+                         (= id :enemies)
                      [(comp/get-initial-state Person {:id 3 :name "Fred" :age 11})
-                      (comp/get-initial-state Person {:id 4 :name "Bobby" :age 55})])})}
+                      (comp/get-initial-state Person {:id 4 :name "Bobby" :age 55})]
+                         (= id :rebels)
+                     [(comp/get-initial-state Person {:id 1 :name "Sally" :age 32})
+                      (comp/get-initial-state Person {:id 5 :name "Bo" :age 22})])})}
   (let [delete-person (fn [person-id] (comp/transact! this [(api/delete-person {:list/id id :person/id person-id})]))] ; (4)
     (dom/div
       (dom/h4 label)
@@ -38,7 +42,8 @@
                    {:friends (comp/get-query PersonList)}
                    {:enemies (comp/get-query PersonList)}]
    :initial-state (fn [params] {:friends (comp/get-initial-state PersonList {:id :friends :label "Friends"})
-                                :enemies (comp/get-initial-state PersonList {:id :enemies :label "Enemies"})})}
+                                :enemies (comp/get-initial-state PersonList {:id :enemies :label "Enemies"})
+                                :rebels (comp/get-initial-state PersonList {:id :rebels :label "Rebels"})})}
   (dom/div
     (ui-person-list rebels)
     (ui-person-list friends)
